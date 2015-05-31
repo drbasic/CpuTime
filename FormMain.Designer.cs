73d7f83482ff53a557355253e9a3f080ab9bc4ea
drbasic <drbasic@mail.ru>
2015-05-31 20:35:31 +0600

    Initial Commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CpuTime
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lCurrentTime = new System.Windows.Forms.Label();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Поехали!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lCurrentTime
+            // 
+            this.lCurrentTime.AutoSize = true;
+            this.lCurrentTime.Location = new System.Drawing.Point(178, 17);
+            this.lCurrentTime.Name = "lCurrentTime";
+            this.lCurrentTime.Size = new System.Drawing.Size(35, 13);
+            this.lCurrentTime.TabIndex = 2;
+            this.lCurrentTime.Text = "label1";
+            // 
+            // textOutput
+            // 
+            this.textOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textOutput.Location = new System.Drawing.Point(12, 41);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(440, 209);
+            this.textOutput.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 262);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.lCurrentTime);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FormMain";
+            this.Text = "CPU Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lCurrentTime;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuTime
+{
+    public partial class FormMain : Form
+    {
+        static FormMain()
+        {
+            allowed_executables = new List<Regex>();
+            allowed_executables.Add(new Regex("browser.exe"));
+            allowed_executables.Add(new Regex("chrome.exe"));
+            string[] meaning_regex =
+            {
+                "gpu", "--type=gpu-process",
+                "renderer", "--type=renderer",
+                "plugin", "--type=plugin",
+                "utility", "--type=utility",
+                "main", "",
+            };
+            meanings = new List<MeaningAndFilter>();
+            for (int i = 0; i < meaning_regex.Count(); i += 2)
+            {
+                meanings.Add(new MeaningAndFilter
+                {
+                    Meaning = meaning_regex[i],
+                    Filter = new Regex(meaning_regex[i + 1])
+                });
+            }
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            lCurrentTime.Text = "";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start_at = DateTime.Now;
+            timer1.Start();
+            processes = (
+                from p in QueryProcessInfo()
+                orderby p.ProcessName, p.Id
+                select p
+                ).ToList();
+            btnStop.Enabled = true;
+            var sb = new StringBuilder();
+            foreach (var p in processes)
+            {
+                sb.AppendFormat(
+                    "{2} {0} ({1})",
+                    p.ProcessName,
+                    p.Meaning,
+                    p.Id
+                    );
+                sb.AppendLine();
+            }
+            textOutput.Text = sb.ToString();
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stop_at = DateTime.Now;
+            timer1.Stop();
+            btnStop.Enabled = false;
+
+            var processes2 = QueryProcessInfo();
+
+            var join = from p2 in processes2
+                       from p1 in processes.Where(a => a.Id == p2.Id).DefaultIfEmpty()
+                       orderby p2.ProcessName, p2.Id
+                       let Meaning = (p1 == null) ? GetMeaning(GetCommandLine(p2.Id)) : p1.Meaning
+                       let TotalProcessorTime1 = (p1 == null) ? new TimeSpan() : p1.TotalProcessorTime
+                       select new
+                       {
+                           p2.Id,
+                           p2.ProcessName,
+                           Meaning,
+                           TotalMilliseconds = (p2.TotalProcessorTime - TotalProcessorTime1).TotalMilliseconds
+                       };
+
+            var sb = new StringBuilder();
+            var delta_time = (stop_at - start_at).TotalMilliseconds;
+            foreach (var p in join)
+            {
+                sb.AppendFormat(
+                    "{2} {0} ({1}) {3} мс, {4:0.0}%",
+                    p.ProcessName,
+                    p.Meaning,
+                    p.Id,
+                    (Int64)p.TotalMilliseconds,
+                    100 * p.TotalMilliseconds / delta_time
+                    );
+                sb.AppendLine();
+            }
+            textOutput.Text = sb.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var text = string.Format("Начали замер в {0:HH:mm:ss}, прошло {1:0.0} с", 
+                start_at, (DateTime.Now - start_at).TotalSeconds);
+            lCurrentTime.Text = text;
+
+        }
+
+        private List<ProccessInfo> QueryProcessInfo()
+        {
+            var processes = Process.GetProcesses()
+               .Where(a => FilterProcess(a))
+               .Select(p => TransformProcess(p))
+               .Where(p => p.IsValid)
+               .ToList();
+            foreach(var p in processes)
+            {
+                p.CommandLine = GetCommandLine(p.Id);
+                p.Meaning = GetMeaning(p.CommandLine);
+            }
+            return processes;
+        }
+        private bool FilterProcess(Process process)
+        {
+            try
+            {
+                foreach (var f in allowed_executables)
+                {
+                    if (f.IsMatch(process.MainModule.FileName))
+                        return true;
+                }
+            }
+            catch (Win32Exception)
+            {
+                return false;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private ProccessInfo TransformProcess(Process process)
+        {
+            try
+            {
+                return new ProccessInfo
+                {
+                    IsValid = true,
+                    Id = process.Id,
+                    ProcessName = process.ProcessName,
+                    FileName = process.MainModule.FileName,
+                    TotalProcessorTime = process.TotalProcessorTime,
+                };
+            }
+            catch (InvalidOperationException)
+            {
+            }
+            return new ProccessInfo();
+        }
+        private string GetCommandLine(int processId)
+        {
+            var commandLine = new StringBuilder();
+            using (var searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + processId))
+            {
+                foreach (var @object in searcher.Get())
+                {
+                    commandLine.Append(@object["CommandLine"] + " ");
+                }
+            }
+            return commandLine.ToString();
+        }
+
+        private string GetMeaning(string command_line)
+        {
+            foreach (var m in meanings)
+            {
+                if (m.Filter.IsMatch(command_line))
+                    return m.Meaning;
+            }
+            return "";
+        }
+
+        class MeaningAndFilter
+        {
+            public string Meaning { get; set; }
+            public Regex Filter { get; set; }
+        }
+        class ProccessInfo
+        {
+            public bool IsValid { get; set; }
+            public TimeSpan TotalProcessorTime { get; set; }
+            public int Id { get; set; }
+            public string ProcessName { get; set; }
+            public string CommandLine { get; set; }
+            public string FileName { get; set; }
+
+            public string Meaning { get; set; }
+        }
+
+        private DateTime start_at;
+        private DateTime stop_at;
+        private List<ProccessInfo> processes;
+
+        private static List<Regex> allowed_executables;
+        private static List<MeaningAndFilter> meanings;
+
+
+
+    }
+}
